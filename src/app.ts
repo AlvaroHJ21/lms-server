1c8bddd4a0e18c1062ec4d079b952eadeb5b7824
AlvaroHJ21 <alvarohuaysara@gmail.com>
2024-02-29 18:38:17 -0500

    update origin cors

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use(express.json({ limit: '50mb' })); // con esto indicamos que el limite de
 app.use(cookieParser());
 
 // cors
-app.use(cors({ origin: process.env.ORIGIN }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 // testing api
 app.get(
